3744ddfd86f2ef3167173688f605b6d5fa97dbf5
rsr986 <rsr986@gmail.com>
2024-04-26 11:35:23 +0530

    add controller and remove gradle summary

--- src/main/java/com/nana/myprojectcicdgithub/MyProjectCicdGithubApplication.java
+++ src/main/java/com/nana/myprojectcicdgithub/MyProjectCicdGithubApplication.java
@@ -5,8 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class MyProjectCicdGithubApplication {
 
 	public static void main(String[] args) {
@@ -23,6 +29,16 @@ public class MyProjectCicdGithubApplication {
 
 	public String getStatus() {
 		return "OK";
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+
+	@GetMapping("/{name}")
+	public String greeting(@PathVariable String name) {
+		return "Hello " + name;
 	}
 
 }
